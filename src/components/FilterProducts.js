278e7389af4680be36a551c01cadc9a66ca23ca3
Rashaad Bracley <rsamuelb14@gmail.com>
2022-03-22 15:55:57 -0400

    final commit version 1

--- src/components/FilterProducts.js
+++ src/components/FilterProducts.js
@@ -4,10 +4,15 @@ import React from "react";
 const FilterProducts = (data) => {
   console.log(data);
 
+  
+    const updateProducts = () => {
+      return data.reverse();
+    };
+
   return (
     <>
       <div className="decks">
-        {data.data.map((item, key) => (
+        {data.data.filter().map((item, key) => (
           <div key={key} className="items">
             <h2>{item.name}</h2>
             <a href="./img/brown.jpeg" target="_blank">
@@ -15,9 +20,7 @@ const FilterProducts = (data) => {
             </a>
           </div>
 
-          //   <div key={key}>
-          //   <h2>{item.name}</h2>
-          // </div>
+         
         ))}
       </div>
     </>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,6 @@
 import React from "react";
 import data from "../products.json";
-import ProductsStyle from "../partials/_products.scss"
+import ProductsStyle from "../partials/_products.scss";
 import FilterProducts from "../components/FilterProducts";
 
 const Products = () => {
@@ -33,92 +33,6 @@ const Products = () => {
       <h1 className="price">All Decks $60 | Click on Deck</h1>
       <button onClick={updateProducts}>update</button>
       <FilterProducts data={data} />
-
-      {/* <div className="decks">
-        <div className="items">
-          <h2>The Niels</h2>
-          <a href="./img/brown.jpeg" target="_blank">
-            <img src="../img/brown.jpeg" alt="brown" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Sad Girl</h2>
-          <a href="./img/sad_girl.jpeg" target="_blank">
-            <img src="./img/sad_girl.jpeg" alt="SAD GIRL" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Niels Retro</h2>
-          <a href="./img/nine_three.jpeg" target="_blank">
-            <img src="./img/nine_three.jpeg" alt="three_nine" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Minty Girl</h2>
-          <a href="./img/mint_green.jpeg" target="_blank">
-            <img src="./img/mint_green.jpeg" alt="mint green" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Mango Girl</h2>
-          <a href="./img/girl_bluOrange.jpeg" target="_blank">
-            <img src="./img/girl_bluOrange.jpeg" alt="blue_orange" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Gass Pointe</h2>
-          <a href="./img/grass_pin.jpeg" target="_blank">
-            <img src="./img/grass_pin.jpeg" alt="grass" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Open Book</h2>
-          <a href="./img/book.jpeg" target="_blank">
-            <img src="./img/book.jpeg" alt="book" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Rise Up</h2>
-          <a href="./img/blm.jpeg" target="_blank">
-            <img src="./img/blm.jpeg" alt="rise up" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>Chocolate Mint</h2>
-          <a href="./img/coco.jpeg" target="_blank">
-            <img src="./img/coco.jpeg" alt="chocolate" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Niels Retro Black</h2>
-          <a href="./img/nine_three_black.jpeg" target="_blank">
-            <img src="./img/nine_three_black.jpeg" alt="retro" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Carolina Girl</h2>
-          <a href="./img/carolina_blu.jpeg" target="_blank">
-            <img src="./img/carolina_blu.jpeg" alt="carolina girl" />
-          </a>
-        </div>
-
-        <div className="items">
-          <h2>The Tangerine</h2>
-          <a href="./img/orange.jpeg" target="_blank">
-            <img src="./img/orange.jpeg" alt="tangerine" />
-          </a>
-        </div>
-      </div> */}
     </>
   );
 };
