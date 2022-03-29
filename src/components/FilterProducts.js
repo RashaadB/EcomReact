3d089c49afa9c0aec83f9bda8b90b59670fef6de
Rashaad Bracley <rsamuelb14@gmail.com>
2022-03-29 14:33:16 -0400

    please work

--- src/components/FilterProducts.js
+++ src/components/FilterProducts.js
@@ -18,7 +18,7 @@ class FilterProducts extends Component {
   }
   //react lifestyle fetching api take response turn into json
   componentDidMount() {
-    fetch("https://rsb-skate-shop-heroku.herokuapp.com", {
+    fetch("https://rsb-skate-shop-heroku.herokuapp.com/", {
       mode: "cors",
       credentials: "true",
     })
