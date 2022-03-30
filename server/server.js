dd4a706e77b681feb39d80cda625baf29bc528b2
Rashaad Bracley <rsamuelb14@gmail.com>
2022-03-30 02:02:03 -0400

    css update

--- server/server.js
+++ server/server.js
@@ -4,17 +4,19 @@ const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-// app.use(cors( res ) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
-//   res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
 //create connection
+// const db = mysql.createConnection({
+//   user: "be1d00d0ff3505",
+//   host: "us-cdbr-east-05.cleardb.net",
+//   password: "1ed6fec1",
+//   database: "heroku_bb146e3c6412d82",
+// });
 const db = mysql.createConnection({
-  user: "be1d00d0ff3505",
-  host: "us-cdbr-east-05.cleardb.net",
-  password: "1ed6fec1",
-  database: "heroku_bb146e3c6412d82",
+  user: "localhost",
+  host: "root",
+  password: "Playsmart1414!!!",
+  database: "rsb_skateshop",
 });
 
 //connect
@@ -36,11 +38,10 @@ app.get("/getpost", (req, res) => {
   });
 });
 
-// app.listen(3001, () => {
-//   console.log("Port is running on 3001!! ");
-// });
-
-app.listen(process.env.PORT || 8080, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(3001, () => {
+  console.log("Port is running on 3001!! ");
 });
 
+// app.listen(process.env.PORT || 8080, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
