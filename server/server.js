414113d29c3e59324942148d727ab149c0aaa7a0
Rashaad Bracley <rsamuelb14@gmail.com>
2022-03-30 06:57:54 -0400

    update procfile

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get("/getpost", (req, res) => {
 //   console.log("Port is running on 3001!! ");
 // });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 8080, function() {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
