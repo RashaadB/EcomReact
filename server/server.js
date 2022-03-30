9dee851b6955d6ba3073501b26a8df4275dc4d64
Rashaad Bracley <rsamuelb14@gmail.com>
2022-03-30 04:53:10 -0400

    port update

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const db = mysql.createConnection({
   database: "heroku_bb146e3c6412d82",
 });
 
-
 //connect
 db.connect((err) => {
   if (err) {
@@ -37,6 +36,10 @@ app.get("/getpost", (req, res) => {
 //   console.log("Port is running on 3001!! ");
 // });
 
-app.listen(process.env.PORT || 8080, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
