9802b9e6c306824c42685382a8a0644d6a395901
Rashaad Bracley <rsamuelb14@gmail.com>
2022-03-31 15:33:16 -0400

    update fetch api for products/getpost

--- src/components/FilterProducts.js
+++ src/components/FilterProducts.js
@@ -13,7 +13,7 @@ class FilterProducts extends React.Component {
   }
   //react lifestyle fetching api take response turn into json
   componentDidMount() {
-    fetch("http://localhost:8080/getpost", {
+    fetch("https://exquisite-faloodeh-ab6fc2.netlify.app/products/getpost/", {
       mode: "cors",
       credentials: "true",
     })
